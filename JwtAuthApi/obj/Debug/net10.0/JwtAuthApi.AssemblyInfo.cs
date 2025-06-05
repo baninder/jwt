b8164276efd0d679b002d12eba623d2160e02fe6
Jhotaa <baninderpal@yahoo.com>
2025-06-04 18:09:35 -0700

    v 0.0.3

--- JwtAuthApi/obj/Debug/net10.0/JwtAuthApi.AssemblyInfo.cs
+++ JwtAuthApi/obj/Debug/net10.0/JwtAuthApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JwtAuthApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c907a452aedafb4d845269120f33790fd66a720")]
 [assembly: System.Reflection.AssemblyProductAttribute("JwtAuthApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JwtAuthApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
